cfe28183222751722bdea9d772e84a711e5707e4
BLJS2warchief <saibalaji.meruva@gmail.com>
2019-09-07 20:01:56 +0530

    Initial commit of the whole code

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float Slowness = 10f;
+
+    public void EndGame()
+    {
+        StartCoroutine("RestartLevel");
+        Debug.Log("Ending Game!!!");
+    }
+
+    IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / Slowness;
+        Time.fixedDeltaTime /= Slowness;
+        Debug.Log("cvbjuygbnjuyg");
+        yield return new WaitForSeconds(1f / Slowness);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime *= Slowness;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+    public float mapWidth = 6f;
+    public float speedCoefficient = 1f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            rb.position = new Vector2(-6, rb.position.y);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.position = new Vector2(-3, rb.position.y);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            rb.position = new Vector2(0, rb.position.y);
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            rb.position = new Vector2(3, rb.position.y);
+        }
+        else if (Input.GetKey(KeyCode.T))
+        {
+            rb.position = new Vector2(6, rb.position.y);
+        }
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<GameManager>().EndGame();
+    }
+}
